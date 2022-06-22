d87b2655e438de5dd30a8ce1435495ad7d1140cd
Blro <gyeongtae020319@gmail.com>
2022-06-22 21:53:24 +0900

    feat: 프로젝트 탐색 추가

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { startCase } from "lodash-es";
+
+const examples = useRouter()
+  .getRoutes()
+  .filter((route) => {
+    return route.path.startsWith("/exam");
+  })
+  .map(({ name, path }) => ({
+    name: name.toString().replace("exam", ""),
+    to: path,
+  }));
+</script>
+
+<template>
+  <div class="page">
+    <ul class="list">
+      <li v-for="{ name, to } in examples" class="list__item">
+        <NuxtLink :to="to">
+          {{ startCase(name) }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
