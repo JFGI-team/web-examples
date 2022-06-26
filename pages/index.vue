bf9147d9bc2a0bea607490629534e5c5fbb7d43c
Blro <gyeongtae020319@gmail.com>
2022-06-26 22:27:50 +0900

    feat: 예시 페이지 링크 연결 추가
    WIP: 스타일을 꾸미지 않음

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const Examples = [
+  { to: "/example/counter", name: "계수기 - Vue: ref" },
+  { to: "/example/calc", name: "계산기 - Vue: ref, computed" },
+  { to: "/example/pointer", name: "포인터 - Vue: ref, v-on" },
+  { to: "/example/cursor-style", name: "커서 스타일 - CSS: cursor" },
+];
+</script>
 
 <template>
-  <div></div>
+  <div class="page">
+    <ol class="list">
+      <li v-for="example in Examples" :key="example.to" class="item">
+        <NuxtLink :to="example.to" class="link">{{ example.name }}</NuxtLink>
+      </li>
+    </ol>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+}
+
+.link {
+  color: #0969da;
+}
+</style>
